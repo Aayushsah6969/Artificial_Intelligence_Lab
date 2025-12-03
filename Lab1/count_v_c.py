4d20e92d87ced15b186041f8d0adc406cfd0725b
Aayush Sah <aayushsah269@gmail.com>
2025-12-03 23:03:27 +0530

    minor update

--- Lab1/count_v_c.py
+++ Lab1/count_v_c.py
@@ -2,14 +2,14 @@ str=input("Enter a string: ")
 # print(type(str))
 v=0
 c=0
-for s in str:
- if(s=='a' or s=='e' or s=='i' or s=='o' or s=='u'):
-#   print(f"{s} is a vowel")
-  v=v+1
- else:
-    # print(f"{s} is a consonant")
-    c=c+1
-print(f"{v} vowels and {c} consonants found in the string.")
+# for s in str:
+#  if(s=='a' or s=='e' or s=='i' or s=='o' or s=='u'):
+#    print(f"{s} is a vowel")
+#   v=v+1
+#  else:
+#      print(f"{s} is a consonant")
+#     c=c+1
+# print(f"{v} vowels and {c} consonants found in the string.")
 
 print("Approach 2: ")
 v=0
